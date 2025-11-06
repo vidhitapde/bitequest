89a51010a43c9d0ecf9a99fe4c3f7cbddcdb534a
Rachel Chan <rchan2023@gmail.com>
2025-11-06 11:57:00 -0800

    fix error

--- app/(tabs)/login.tsx
+++ app/(tabs)/login.tsx
@@ -5,7 +5,7 @@ import { Button } from '@/components/ui/button';
 import { Text } from '@/components/ui/text';
 import { Input } from "@/components/ui/input"
 import { Checkbox } from "@/components/ui/checkbox";
-import { Label } from '@/components/ui/label';
+import { Label } from "@/components/ui/label";
 import * as Haptics from 'expo-haptics';
 import React from "react";
 
